e3f61b16acbf01725f030f7ce9739d20fff7d91d
MrDGuy <84729967+MrDGuy@users.noreply.github.com>
2023-09-06 21:00:08 -0400

    Update tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -24,15 +24,15 @@ namespace myTiles {
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "level1":
-            case "level2":return tiles.createTilemap(hex`0a0007000404040a050a040a050b09040d020e0e0e110d040504130d050303140d0c09100e0e0e0e0e0f05040414030d030d0d0d0d0c09120e0e140e0e0e010c07060606060606060608`, img`
+            case "level2":return tiles.createTilemap(hex`0a0007000404040a050a040a050b09010d0d0d0d050d0d04051403100e0e13110d0c0912130f0d05051211040404040410131103140c09020e0e0f03120e0f0c07060606060606060608`, img`
 2 2 2 2 2 2 2 2 2 2 
-2 2 2 . . . . . 2 2 
-2 2 2 2 2 2 2 . 2 2 
-2 . . . . . . . 2 2 
 2 . 2 2 2 2 2 2 2 2 
-2 . . . . . . . . 2 
+2 . 2 . . . . . 2 2 
+2 . . . 2 2 2 . . 2 
+2 2 2 2 . . . 2 . 2 
+2 . . . . 2 . . . 2 
 2 2 2 2 2 2 2 2 2 2 
-`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile8,myTiles.tile9,myTiles.tile10,sprites.skillmap.islandTile7,sprites.skillmap.islandTile6,sprites.skillmap.islandTile8,sprites.skillmap.islandTile3,sprites.skillmap.islandTile1,sprites.skillmap.islandTile2,sprites.skillmap.islandTile5,sprites.skillmap.islandTile4,sprites.vehicle.roadHorizontal,sprites.vehicle.roadTurn4,sprites.vehicle.roadTurn1,sprites.vehicle.roadTurn2,sprites.vehicle.roadTurn3,myTiles.tile11,myTiles.tile3], TileScale.Sixteen);
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile8,myTiles.tile9,myTiles.tile10,sprites.skillmap.islandTile7,sprites.skillmap.islandTile6,sprites.skillmap.islandTile8,sprites.skillmap.islandTile3,sprites.skillmap.islandTile1,sprites.skillmap.islandTile2,sprites.skillmap.islandTile5,sprites.skillmap.islandTile4,sprites.vehicle.roadHorizontal,sprites.vehicle.roadTurn4,sprites.vehicle.roadTurn1,sprites.vehicle.roadTurn2,sprites.vehicle.roadTurn3,myTiles.tile3,sprites.vehicle.roadVertical], TileScale.Sixteen);
         }
         return null;
     })
